39e4940769f48315e0461cd7586f5638f3684ac2
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-12-30 14:08:33 -0600

    javascript practice function

--- js/external.js
+++ js/external.js
@@ -1,50 +1,63 @@
 "use strict";
 
-console.log("Hello from External JS!");
+// console.log("Hello from External JS!");
+//
+// alert("Welcome to my website!");
+//
+// var userColor = prompt("What is your favorite color?");
+//
+// userColor;
+//
+// alert("Great, " + userColor + " is my favorite color too!");
+//
+// // exercise 3 from previous lesson
+//
+// var little_mermaid = 3;
+// var brother_bear = 5;
+// var hercules = 1;
+// var price_perday = 3;
+//
+// console.log("Total price of rental is $" + ((little_mermaid + brother_bear + hercules) * price_perday) + ".");
+//
+// var google = 400;
+// var amazon = 380;
+// var facebook = 350;
+//
+// var google_hours = prompt("How many hours did you work for Google?");
+// var amazon_hours = prompt("How many hours did you work for Amazon?");
+// var facebook_hours = prompt("How many hours logged for FaceBook?");
+// var paycheck = ((google * google_hours) + (amazon * amazon_hours) + (facebook * facebook_hours));
+//
+// alert("Total paycheck is $" + (google * google_hours + amazon * amazon_hours + facebook * facebook_hours) + ".");
+//
+// console.log(paycheck);
+//
+// //confirm method returns a boolean value and a prompt method returns string entered by user
+// var premium_member = confirm("Are you a premium member?");
+// var product_offer = confirm("Is the offer still valid");
+// var number_of_products = confirm("Are you buying more than two products?");
+// console.log(premium_member);
+// console.log(product_offer);
+// console.log(number_of_products);
+//
+// if((premium_member == true || number_of_products == true) && number_of_products == true){
+//     console.log("yes");
+//     alert("offer applied");
+// } else {
+//     console.log("no");
+//     alert("no");
+//
+// }
 
-alert("Welcome to my website!");
-
-var userColor = prompt("What is your favorite color?");
-
-userColor;
-
-alert("Great, " + userColor + " is my favorite color too!");
-
-// exercise 3 from previous lesson
-
-var little_mermaid = 3;
-var brother_bear = 5;
-var hercules = 1;
-var price_perday = 3;
-
-console.log("Total price of rental is $" + ((little_mermaid + brother_bear + hercules) * price_perday) + ".");
-
-var google = 400;
-var amazon = 380;
-var facebook = 350;
-
-var google_hours = prompt("How many hours did you work for Google?");
-var amazon_hours = prompt("How many hours did you work for Amazon?");
-var facebook_hours = prompt("How many hours logged for FaceBook?");
-var paycheck = ((google * google_hours) + (amazon * amazon_hours) + (facebook * facebook_hours));
-
-alert("Total paycheck is $" + (google * google_hours + amazon * amazon_hours + facebook * facebook_hours) + ".");
-
-console.log(paycheck);
-
-//confirm method returns a boolean value and a prompt method returns string entered by user
-var premium_member = confirm("Are you a premium member?");
-var product_offer = confirm("Is the offer still valid");
-var number_of_products = confirm("Are you buying more than two products?");
-console.log(premium_member);
-console.log(product_offer);
-console.log(number_of_products);
-
-if((premium_member == true || number_of_products == true) && number_of_products == true){
-    console.log("yes");
-    alert("offer applied");
-} else {
-    console.log("no");
-    alert("no");
-
+function userNumber(){
+    var userOutput = prompt("Enter a number");
+    return parseInt(userOutput);
 }
+
+function incrementNum(x){
+    return (x + 1);
+}
+var num = incrementNum(userNumber());
+console.log(num);
+
+
--- js/javascript_for_Ethan_pic.js
+++ js/javascript_for_Ethan_pic.js
@@ -2,12 +2,26 @@
 
 alert("Welcome to Ethan's Picture Page.");
 
-var registered_user = confirm("Are you a Coach?");
+// var registered_user = confirm("Are you a Coach?");
+//
+// if (registered_user == true){
+//     alert("Great!");
+//     var coach_email = prompt("Please enter your email address.");
+//     console.log(coach_email);
+// } else {
+//     alert("Thank you for visiting!");
+// }
 
-if (registered_user == true){
-    alert("Great!");
-    var coach_email = prompt("Please enter your email address.");
-    console.log(coach_email);
-} else {
-    alert("Thank you for visiting!");
+function coachEmail(){
+    var coach = confirm("Are you a coach?");
+    if (coach == true){
+        alert("Great!");
+        var coachEmail = prompt("Please enter your email address");
+        console.log(coachEmail);
+        alert(coachEmail);
+    } else {
+        alert("Thank you for visiting!");
+    }
 }
+
+coachEmail();
