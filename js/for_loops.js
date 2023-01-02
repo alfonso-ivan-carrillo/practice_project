fac20a16837b0f58c111f21d102842c79adf0dff
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2023-01-01 20:57:30 -0600

    javascript practice loops exercises problem 2 completed

--- js/for_loops.js
+++ js/for_loops.js
@@ -17,6 +17,27 @@ function showMultiplicationTable(number){
 }
 showMultiplicationTable(userNumber());
 
+function generateNums(){
+    // for(var i = 0; i < 10; i++){
+    var luckyNumber = Math.floor(Math.random() * (201 - 20) + 20);
+    // console.log(luckyNumber);
+    return(luckyNumber);
+    // }
+}
+generateNums();
 
+function numOddEven(){
+    for (var i = 0; i < 10; i++){
+        var nums = generateNums();
+        // console.log(nums);
+        if(nums % 2 == 0){
+            console.log(nums + " is even.");
+        } else {
+            console.log(nums + " is odd.");
+        }
+    }
+
+}
+numOddEven();
 
 })();
