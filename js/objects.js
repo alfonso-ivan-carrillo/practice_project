3967df7109ff37ce4519b15ae881dce8d745ff73
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2023-01-04 16:32:41 -0600

    javascript practice object exercises completed

--- js/objects.js
+++ js/objects.js
@@ -1,63 +1,62 @@
 (function() {
-    "use strict";
+"use strict";
 
-    var car = new Object();
-    console.log("car " + typeof car);
-    car.make = "toyota";
-    car["model"] = "tacoma";
-    car.honk = function (){
-        alert("Honk! Honk!");
-    }
-    console.log(car["make"]);
-    console.log(car.model);
-    car.honk();
+var car = new Object();
+console.log("car " + typeof car);
+car.make = "toyota";
+car["model"] = "tacoma";
+car.honk = function (){
+    alert("Honk! Honk!");
+}
+console.log(car["make"]);
+console.log(car.model);
+car.honk();
 
-    var heros = [{                // curly braces also creates new object
-        name: "woverine",
-        team: "x-men",
-        powers: ["healing", "claws", "animal senses"],
-        secretIdentity: {
-            alias: "logan",
-            job: "fighter"
-        },
-        action: function (){
-            alert("Snkt!");
-        },
-        activate: function (){
-            console.log("Woverline: \"You messed up bub.\" " + this.action());
-        }
-
+var heros = [{                // curly braces also creates new object
+    name: "woverine",
+    team: "x-men",
+    powers: ["healing", "claws", "animal senses"],
+    secretIdentity: {
+        alias: "logan",
+        job: "fighter"
     },
-        {
-        name: "deadpool",
-        team: "x-force",
-        powers: ["healing", "weapons master", "humor"],
-        secretIdentity: {
-            alias: "wade",
-            job: "merc"
-        },
-        action: function () {
-            alert("Pow Pow!");
-        },
-        activate: function (){
-            console.log("Deadpool: \"Ha Ha!\" " + this.activate());
-        }
+    action: function (){
+        alert("Snkt!");
+    },
+    activate: function (){
+        console.log("Woverline: \"You messed up bub.\" " + this.action());
+    }
 
-    }];
-    console.log("The name of the first hero is " + heros[0].name + ".");
-    console.log("The name of the second hero is " + heros[1].name + ".");
+},
+    {
+    name: "deadpool",
+    team: "x-force",
+    powers: ["healing", "weapons master", "humor"],
+    secretIdentity: {
+        alias: "wade",
+        job: "merc"
+    },
+    action: function () {
+        alert("Pow Pow!");
+    },
+    activate: function (){
+        console.log("Deadpool: \"Ha Ha!\" " + this.activate());
+    }
+}];
+console.log("The name of the first hero is " + heros[0].name + ".");
+console.log("The name of the second hero is " + heros[1].name + ".");
 
-    heros.forEach(function (hero){
-        console.log(hero.name + " : " + hero.team);
+heros.forEach(function (hero){
+    console.log(hero.name + " : " + hero.team);
+})
+heros.forEach(function (hero){
+    hero.powers.forEach(function (power){
+        console.log(power);
     })
-    heros.forEach(function (hero){
-        hero.powers.forEach(function (power){
-            console.log(power);
-        })
-    })
+})
     // heros[0].action();
     // heros[1].action();
-    heros[0].activate();
+heros[0].activate();
 
 
 
@@ -72,14 +71,14 @@
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
-        var person = {
-            firstName: "Alfonso",
-            lastName: "Carrillo",
-            sayHello: function (){
-                console.log("Hello from " + this.firstName + " " + this.lastName + ".");
-            }
+var person = {
+    firstName: "Alfonso",
+    lastName: "Carrillo",
+    sayHello: function (){
+        console.log("Hello from " + this.firstName + " " + this.lastName + ".");
     }
-    console.log(person.firstName + " " + person.lastName);
+}
+console.log(person.firstName + " " + person.lastName);
 
 
     /**
@@ -91,7 +90,7 @@
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
-    person.sayHello();
+person.sayHello();
 
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
@@ -107,23 +106,23 @@
      * and console.log the relevant messages for each person
      */
 
-    var shoppers = [
-        {name: 'Cameron', amount: 180},
-        {name: 'Ryan', amount: 250},
-        {name: 'George', amount: 320}
-    ];
+var shoppers = [
+    {name: 'Cameron', amount: 180},
+    {name: 'Ryan', amount: 250},
+    {name: 'George', amount: 320}
+];
 
-    function hebDiscount(){
-        var discount = .12;
-        shoppers.forEach(function (shopper){
-            if (shopper.amount >= 200){
-                console.log(shopper.name + " qualified for a discount of 12%. He paid $" + shopper.amount + " but after discount is applied the shopper will pay $" + (shopper.amount - (shopper.amount * discount)) + ".");
-            } else {
-                console.log(shopper.name + " did not qualify for a discount. The shopper will pay the full price of $" + shopper.amount + ".");
-            }
-        })
-    }
-    hebDiscount();
+function hebDiscount(){
+    var discount = .12;
+    shoppers.forEach(function (shopper){
+        if (shopper.amount >= 200){
+            console.log(shopper.name + " qualified for a discount of 12%. He paid $" + shopper.amount + " but after discount is applied the shopper will pay $" + (shopper.amount - (shopper.amount * discount)).toFixed(2) + ".");
+        } else {
+            console.log(shopper.name + " did not qualify for a discount. The shopper will pay the full price of $" + shopper.amount + ".");
+        }
+    })
+}
+hebDiscount();
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
@@ -138,41 +137,41 @@
      * > console.log(books[0].author.lastName) // "Adams"
      */
 
-    var books = [{
-            title: "The Road",
-            author: {
-                firstName: "Cormac",
-                lastName: "McCarthy"
-            }
-        },
-        {
-            title: "Fight Club",
-            author: {
-                firstName: "Chuck",
-                lastName: "Palanhck"
-            }
-        },
-        {
-            title: "A Game of Thornes",
-            author: {
-                firstName: "George",
-                lastName: "Martin"
-            }
-        },
-        {
-            title: "The First Law",
-            author: {
-                firstName: "Joe",
-                lastName: "Abercombie"
-            }
-        },
-        {
-            title: "John Dies at the End",
-            author: {
-                firstName: "James",
-                lastName: "Wong"
-            }
-        }];
+var books = [{
+        title: "The Road",
+        author: {
+            firstName: "Cormac",
+            lastName: "McCarthy"
+        }
+    },
+    {
+        title: "Fight Club",
+        author: {
+            firstName: "Chuck",
+            lastName: "Palanhck"
+        }
+    },
+    {
+        title: "A Game of Thornes",
+        author: {
+            firstName: "George",
+            lastName: "Martin"
+        }
+    },
+    {
+        title: "The First Law",
+        author: {
+            firstName: "Joe",
+            lastName: "Abercombie"
+        }
+    },
+    {
+        title: "John Dies at the End",
+        author: {
+            firstName: "James",
+            lastName: "Wong"
+        }
+    }];
     /**
      * TODO:
      * Loop through the books array and output the following information about
@@ -197,10 +196,10 @@
      *      ---
      *      ...
      */
-    for (var i = 0; i < books.length ; i++){
-        console.log("Book # " + (i+  1));
-        console.log("Title: " + books[i].title);
-        console.log("Author: " + books[i].author.firstName + " " + books[i].author.lastName);
+for (var i = 0; i < books.length ; i++){
+    console.log("Book # " + (i+  1) + "\nTitle: " + books[i].title + "\nAuthor: " + books[i].author.firstName + " " + books[i].author.lastName);
+        // console.log("Title: " + books[i].title);
+        // console.log("Author: " + books[i].author.firstName + " " + books[i].author.lastName);
     }
     /**
      * Bonus:
@@ -212,5 +211,31 @@
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
      */
+var bookCollection = [];  // create an array to store books created from function
+function createBook(title, author){
+    var authorName = author.split(" ");     // split author's name since it stored as an object
+    var book = {
+        title: title,
+        author: {
+            firstName: authorName[0],       // since author's name was split into an array, access value as an array object
+            lastName: authorName[1]
+        }
+    }
+    return bookCollection.push(book);
+}
+
+createBook("No Country for Old Men", "Cormac McCarthy");
+createBook("Clash of Kings", "George Martin");
+console.log(bookCollection);
+
+function showBookInfo(books){
+    for (var i = 0; i < books.length; i++){
+        console.log("Book # " + ( i + 1)  + "\nTitle: " + books[i].title + "\nAuthor: " + books[i].author.firstName + " " + books[i].author.lastName );
+    }
+}
+showBookInfo(books);
+
+
+
 
 })();
