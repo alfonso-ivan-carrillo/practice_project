9f7a0fe1ba1718015ffbf44f6735536fff67796c
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2023-01-10 21:48:27 -0600

    javascript practice coffee project html label search added

--- js/main.js
+++ js/main.js
@@ -1,41 +1,42 @@
 "use strict"
 
-function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
-
-    return html;
-}
+// function renderCoffee(coffee) {
+//     var html = '<tr class="coffee">';
+//     html += '<td>' + coffee.id + '</td>';
+//     html += '<td>' + coffee.name + '</td>';
+//     html += '<td>' + coffee.roast + '</td>';
+//     html += '</tr>';
+//
+//     return html;
+// }
 
 function renderCoffeeDiv(coffee){
-    var html = "<div class='coffeeDiv'>";
-     html += "<div> " + coffee.id + "</div>";
-     html += "<div>" + coffee.name + "</div>";
-     html += "<div>" + coffee.roast + "</div>";
-    html += "</div>";
+    var html = "<div class='coffeeDiv row'>";
+     // html += "<div> " + coffee.id + "</div>";
+     html += "<div><h2>" + coffee.name + "</h2></div>";
+     html += "<div><small>" + coffee.roast + "</small></div>";
+     html += "</div>";
+
+    return html;
 }
 
 function renderCoffeesDivs(coffees) {
     var html = '';
-    // for(var i = coffees.length - 1; i >= 0; i--) {  // this renders the array in descending order due to how the loop is setup
     for (var i = 0; i < coffees.length - 1; i++){
-        html += renderCoffee(coffees[i]);
+        html += renderCoffeeDiv(coffees[i]);
     }
     return html;
 }
 
 
-function renderCoffees(coffees) {
-    var html = '';
-    // for(var i = coffees.length - 1; i >= 0; i--) {  // this renders the array in descending order due to how the loop is setup
-    for (var i = 0; i < coffees.length - 1; i++){
-        html += renderCoffee(coffees[i]);
-    }
-    return html;
-}
+// function renderCoffees(coffees) {
+//     var html = '';
+//     // for(var i = coffees.length - 1; i >= 0; i--) {  // this renders the array in descending order due to how the loop is setup
+//     for (var i = 0; i < coffees.length - 1; i++){
+//         html += renderCoffee(coffees[i]);
+//     }
+//     return html;
+// }
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
@@ -46,7 +47,7 @@ function updateCoffees(e) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    coffeeDiv.innerHTML = renderCoffeesDivs(filteredCoffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -67,11 +68,11 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
+// var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
+// tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
 
