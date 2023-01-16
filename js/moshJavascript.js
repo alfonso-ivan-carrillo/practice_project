8a084b1cf1f237b6e18f23aa50248287c8550599
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2023-01-16 16:35:52 -0600

    practice javascript mosh OOP lesson

--- js/moshJavascript.js
+++ js/moshJavascript.js
@@ -2,6 +2,9 @@
 
 (function (){
 
+// TODO: OOP Pilliars - Encapsulation, Abstraction, Inheritance and Polymorphism
+
+
 // TODO: example of procedural programming - variables on one side and functions in another
 
 let baseSalary = 30_000;
@@ -28,6 +31,17 @@ getWage(20000, 10, 20);
     };
     // console.log(employee.getWage());
     employee.getWage();
+
+
+
+// TODO: Abstraction - benfits makes interaction of objects easier to interface and it helps reduce the impact of change. To abstract something away means to hide away the implementation details inside something (the complex methods or moving parts). So when you call the function you don't have to understand exactly what is doing. An example is: The APPEND and INSERT methods of your data structures. Instead of calling keep_going_until_you_find_a_node_that_is_nil_and_create_a_new_node_and_fill_with_this_data, we can call APPEND or INSERT.
+
+
+// TODO: Inheritance - is a mechanism to alow you to get rid of redundant code - Child Classes with inherit all of the behavior and attributes of the parent class.
+//TODO: example of Inheritance -  each html element has similar properties such as hidden or innerHTML or like click(), instead of defining each for each element, you can group these into a parent class, like HTMLElement and then have other elements such as <textbox>, <select> or <checkbox>, inherit from the HTMLElement and be abuse to use these properties/methods.
+
+// TODO: Polymorphism - Call the same method on different objects - example: petting zoo, you can pet all the animals but you can pet them the same way, so you would pet a dog differently than you would a porcupine. You could create a Method to PET an animal, PET(){return petAnimal};, and with polymorphism, you could reuse the the PET Method to another another animal, PET(porcupine){ var glove = true, return petAnimal};.  ---- element.pet()  -- porcupine.pet();
+
 
 
 
