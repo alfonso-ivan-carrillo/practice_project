b6e4708b2d93260119f8ac0f8f2257e37d7978fa
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2023-01-17 10:45:59 -0600

    practice javascript comments added to explain JS built-in object constructors

--- js/moshJSLesson1.js
+++ js/moshJSLesson1.js
@@ -38,8 +38,34 @@
             console.log("Constructor Circle");
         }                                               // todo --> no return is needed, automatically done with constructor
     }
+
     const circleConstruct = new Circle(1);      // todo --> new  keyword initializes a new circle object and does not refer to the global or window object
+    // todo ->    const circleConstruct = new Circle(1); same as using the CALL method to create a new circle object  -- Circle.call({}, 1);
     circleConstruct.draw();
+
+
+
+
+    console.log(circleConstruct.constructor);
+    console.log(circleFactory.constructor);
+
+    console.log(Circle.name);
+    console.log(Circle.length);
+    console.log(Circle.constructor);
+
+                // todo -> Notice Function is capitalized to call internal function that creates objects
+    // const CircleTest = new Function("radius",
+    //     `this.radius = radius;
+    //     this.draw = function(){
+    //     console.log('internal object constructor used to draw')
+    //     };`
+    // );
+    //
+    // const newCircle = new CircleTest(1);
+    // newCircle.draw();
+    // console.log(newCircle);
+
+
 
 
 }());
