98d66d81c78a510969ea31faf89c62801ec45c68
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2023-01-17 10:12:16 -0600

    practice javascript differences between factory and constructor object creation

--- js/moshJSLesson1.js
+++ js/moshJSLesson1.js
@@ -19,18 +19,27 @@
     console.log(circle.radius);
 
     // todo -> Factories - or constructor function
-
     function createCircle(radius){
-        return {
+        return {                                    // todo --> if the function returns an object, then you are using a factory
             radius: radius,
             draw: function (){
                 console.log('Draw One');
             }
         };
     }
-    const circleOne = createCircle(1);
-    circleOne.draw();
-    console.log(circleOne.radius);
+    const circleFactory = createCircle(1);
+    circleFactory.draw();
+    console.log(circleFactory.radius);
+
+    // todo Constructor Function
+    function Circle(radius){
+        this.radius = radius;                           // todo -->  this.  refers to the circle object being created
+        this.draw = function (){
+            console.log("Constructor Circle");
+        }                                               // todo --> no return is needed, automatically done with constructor
+    }
+    const circleConstruct = new Circle(1);      // todo --> new  keyword initializes a new circle object and does not refer to the global or window object
+    circleConstruct.draw();
 
 
 }());
