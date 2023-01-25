ff8fdde941058214c927623e92da2bd3a668b56d
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2023-01-25 00:22:39 -0600

    practice javascript mosh lesson / coaches corner practice

--- js/futbolCoach.js
+++ js/futbolCoach.js
@@ -2,25 +2,50 @@
 
 
 (function (){
+
+    var coaches = [{
+        id: 0,
+        name: "David Bauer",
+        college: "Schreiner University"
+    },{
+        id: 1,
+        name: "Brady Van Holland",
+        college: "LeTourneau University"
+    }, {
+        id: 2,
+        name: "Matt Pitcock",
+        college: "Hardin-Simmons University"
+    }];
     function helloCoach(){
+        let tempArray = [];
+
+        let tempCoachObject = {
+            id: "",
+            name: "something",
+            college: "somethingU"
+        }
+
         alert("Welcome to the Coach's Corner!");
-        let idCoach = prompt("Are you a coach?");
+        let inputName = prompt("name");
+
+        let inputCollege = prompt("college");
+        tempCoachObject.id = coaches.length + 1;
+        tempCoachObject.name = inputName;
+
+
+
+        tempCoachObject.college = inputCollege;
+        coaches.push(tempCoachObject);
+
+        console.log(coaches);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    helloCoach();
 
 
 
 }());
+
+
+
+
