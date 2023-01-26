b1d07219390fe297fa12fd85a5bee2333763a6b7
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2023-01-26 17:09:01 -0600

    practice javascript jQuery lesson / .toggle between images

--- js/jQueryEventhandler.js
+++ js/jQueryEventhandler.js
@@ -3,10 +3,12 @@
 $(document).ready(function (){
     alert("Page is loaded.");
 
+    $('li').css('list-style-type', 'none');
+
 //todo  .clck()
     $('#villainsHeader').click(function (){
         $('#villainsHeader').html("Killer Characters");
-    })
+    });
 
     let anton = $('li').eq(0);
     anton.click(function (){
@@ -49,7 +51,12 @@ $(document).ready(function (){
         }, function (){
             $(this).attr('src', 'images/agent-smith-matrix.webp');
         }
-    )
+    );
+
+//todo  toggle images
+    $('.hereditaryImage').click(function (){    //todo  grab class .hereditaryImage - 2 images on html - 1 displayed and other hidden - use .toggle to switch back and forth (display one and hide the other)
+        $('img', this).toggle();
+    });
 
 
 
